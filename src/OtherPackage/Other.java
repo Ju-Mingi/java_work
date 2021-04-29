ebb7551738c316b5b1c90ee939c16df3eb7d4d35
Ju-Mingi <jmg961218@gmail.com>
2021-04-29 17:19:57 +0900

    210429 접근제한자 , 자바 연습 문제

--- src/OtherPackage/Other.java
+++ src/OtherPackage/Other.java
@@ -1,6 +1,20 @@
 package OtherPackage;
 
 public class Other {
+	private void _private() {
+		System.out.println("private");
+	}
+	
+	void _default() {
+		System.out.println("default");
+	}
+	
+	protected void _protected() {
+		System.out.println("protected");
+	}
+	public void _public() {
+		System.out.println("public");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
