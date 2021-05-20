55e13ff82187b1be439bf6dd74b36d769725c6bd
Ju-mingi <jmg961218@gmail.com>
2021-05-20 11:37:07 +0900

    210520 throw 예외처리

--- src/exception/CalculatorDemo.java
+++ src/exception/CalculatorDemo.java
@@ -4,11 +4,20 @@ class Calculator {
 	int left, right;
 	
 	public void setOperands(int left, int right) {
+		
+		/* 예외 만들기 1
+		 * if (right == 0) { throw new
+		 * IllegalArgumentException("두 번째 인자 값은 0이 될 수 없습니다."); }
+		 */
 		this.left = left;
 		this.right = right;
 	}
 	
 	public void divide() {
+		// divide 에서 예외 만들기
+		if(this.right == 0) {
+			throw new ArithmeticException("0으로 나누는 것은 허용되지 않습니다.");
+		}
 		try {
 			System.out.print("계산결과는 ");
 			System.out.print(this.left / this.right);
@@ -30,7 +39,11 @@ public class CalculatorDemo {
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator();
 		c1.setOperands(10, 0);
-		c1.divide();
+		try{
+			c1.divide();
+		}catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
--- src/exception/ThrowExceptionDemo.java
+++ src/exception/ThrowExceptionDemo.java
@@ -2,30 +2,32 @@ package exception;
 
 import java.io.*;
 
-class B{
-	void run() throws FileNotFoundException {
+class B {
+	void run() throws FileNotFoundException, IOException {
 		BufferedReader bReader = null;
 		String input = null;
-		try {
-			bReader = new BufferedReader(new FileReader("out.txt"));
-		}
-		catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		try {
-			input = bReader.readLine();
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
+		bReader = new BufferedReader(new FileReader("out.txt"));
+		input = bReader.readLine();
+		
+		/*
+		 * try { bReader = new BufferedReader(new FileReader("out.txt")); } catch
+		 * (FileNotFoundException e) { e.printStackTrace(); }
+		 * 
+		 * try { input = bReader.readLine(); } catch (IOException e) {
+		 * e.printStackTrace(); }
+		 */
 		System.out.println(input);
 	}
 }
 
-class C{
-	void run() {
+class C {
+	void run() throws FileNotFoundException, IOException {
 		B b = new B();
-		b.run(); // class B 의 예외처리를 받음
+		b.run();
+		/*
+		 * try { b.run(); // class B 의 예외처리를 받음 } catch (FileNotFoundException e) {
+		 * e.printStackTrace(); } catch(IOException e) { e.printStackTrace(); }
+		 */
 	}
 }
 
@@ -33,7 +35,13 @@ public class ThrowExceptionDemo {
 
 	public static void main(String[] args) {
 		C c = new C();
-		c.run();
+		try {
+			c.run();
+		}catch(FileNotFoundException e) {
+			System.out.println("out.txt 파일이 필요합니다.");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
