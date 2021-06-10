eb7ca489bcbe064eed84ae3e7146c882f9e57ed0
Ju-Mingi <jmg961218@gmail.com>
2021-06-10 17:50:58 +0900

    21.06.10 java_exam

--- src/java_example/Exam6_11.java
+++ src/java_example/Exam6_11.java
@@ -1,24 +1,32 @@
-// 6- 10 요소의 형이 int형인 배열을 만들어서 모든 요소에 1~10 난수를 대입
-// 인접하는 요소가 동일한 값을 가지지 않도록 6-10 수정하기
 package java_example;
 
 import java.util.Scanner;
 
 import java.util.Random;
 
-public class Exam6_10 {
+public class Exam6_11 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Random rd = new Random();
-		System.out.print("요소수 입력 : ");
-		int input = sc.nextInt();
+		int input;
+		do {
+			System.out.print("요소수 입력 : ");
+			input = sc.nextInt();
+		} while (input > 10);
+
 		int a[] = new int[input];
 		a[0] = 1 + rd.nextInt(10);
 		for (int i = 1; i < input; i++) {
+			int j;
 			do {
 				a[i] = 1 + rd.nextInt(10);
-			} while (a[i] == a[i - 1]);
+				for (j = 0; j < i; j++) {
+					if (a[j] == a[i]) {
+						break;
+					}
+				}
+			} while (j < i);
 		}
 
 		for (int i = 0; i < input; i++) {
